861f2a1b2e007554ec6f711501f1d3a135cd6d3a
DRausseo <dannymrleon@yahoo.com>
2024-06-16 22:54:00 -0300

    Actualizar configuración del proyecto y corregir errores de ruta

--- server.js
+++ server.js
@@ -1,30 +1,49 @@
-/* ******************************************
+/******************************************
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const path = require("path");
+const staticRoutes = require("./routes/static");
+const app = express();
+
+/* ***********************
+ * Middleware
+ *************************/
+app.use(express.static(path.join(__dirname, 'public')));
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); // Ruta del layout principal
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use('/', staticRoutes);
+
+app.get("/", function(req, res){
+  res.render("index", { title: "Home" });
+});
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 5500;
+const host = process.env.HOST || 'localhost';
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`App listening on http://${host}:${port}`);
+});
