ee72210169f12f5ae255c4f3a390765a5cc3b11d
DRausseo <dannymrleon@yahoo.com>
2024-06-29 10:42:54 -0300

    actualizar codigos

--- routes/static.js
+++ routes/static.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 // Static Routes
-router.use(express.static(path.join(__dirname, '../public')));
+router.use(express.static('../public'));
 router.use("/css", express.static(path.join(__dirname, '../public/css')));
 router.use("/js", express.static(path.join(__dirname, '../public/js')));
 router.use("/images", express.static(path.join(__dirname, '../public/images')));
